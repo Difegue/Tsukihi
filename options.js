addde967026bcb499f3594b5b3a322f9b95ff3c5
difegue <sugoi@cock.li>
2021-04-03 16:19:34 +0200

    List categories from server

--- options.js
+++ options.js
@@ -29,6 +29,7 @@ function checkServer() {
 
   let server = document.getElementById('server');
   let key = document.getElementById('key');
+  let selectedID = document.getElementById('catid').value;
 
   let serverInfo = "";
   let apiCheck = "";
@@ -58,6 +59,33 @@ function checkServer() {
     })
     .catch(error => { apiCheck = `🛑 ${error}` })
     .finally(() => document.getElementById('apiCheck').textContent = apiCheck);
+
+  // Get categories from the server 
+  document.getElementById('categoryErr').textContent = "";
+  fetch(`${server.value}/api/categories`, { method: "GET", headers: getAuthHeader(key.value) })
+    .then(response => response.ok ? response.json() : { error: "API Key seems to be invalid!" })
+    .then((data) => {
+
+      if (data.error)
+        throw new Error(data.error);
+
+      // Clear combobox and fill it again with categories from the API
+      const catCombobox = document.getElementById('category');
+      catCombobox.options.length = 0;
+      // Add default
+      catCombobox.options[catCombobox.options.length] = new Option("-- No Category --", "", true, false);
+
+      // Add categories, select if the ID matches the optional argument
+      data.forEach(c => {
+        catCombobox.options[catCombobox.options.length] = new Option(c.name, c.id, false, c.id === selectedID);
+      });
+
+    }).catch(error => document.getElementById('categoryErr').textContent = error);
+}
+
+function updateCategoryDetails() {
+  const categoryID = document.getElementById('category').value;
+  document.getElementById('catid').value = categoryID;
 }
 
 let button = document.getElementById('saveBtn');
@@ -65,6 +93,9 @@ button.addEventListener('click', updateSettings);
 
 button = document.getElementById('checkBtn');
 button.addEventListener('click', checkServer);
+
+button = document.getElementById('category');
+button.addEventListener('change', updateCategoryDetails);
 
 // Prefill fields with settings
 chrome.storage.sync.get(['server', 'api', 'categoryID'], function (result) {
